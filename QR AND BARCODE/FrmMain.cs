7a8efc7293d1789ced09962067169a51eb92fb31
nara- <nara-@DESKTOP-F8VK5UN>
2019-05-08 14:55:25 +0700

    i am tested

--- QR AND BARCODE/FrmMain.cs
+++ QR AND BARCODE/FrmMain.cs
@@ -20,7 +20,7 @@ namespace QR_AND_BARCODE
         private void Barcode_Click(object sender, EventArgs e)
         {
             Code128BarcodeDraw barcode = BarcodeDrawFactory.Code128WithChecksum;
-            pictureBox1.Image = barcode.Draw(Mybarcode.Text,20);
+            pictureBox1.Image = barcode.Draw(Mybarcode.Text,20111111);
         }
 
         private void qrcode_Click(object sender, EventArgs e)
